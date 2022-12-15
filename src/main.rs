af42714b296df46ef383cc2abafd75203ee91a5c
Nat Budin <nbudin@actbluetech.com>
2022-12-15 10:22:45 -0800

    Refactor a bit, semi-handle errors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use image::{
     imageops::{self, FilterType},
-    DynamicImage, ImageFormat,
+    DynamicImage, ImageError, ImageFormat,
 };
 
 fn resize_crop(img: DynamicImage, width: u32, height: u32) -> DynamicImage {
@@ -10,20 +10,47 @@ fn resize_crop(img: DynamicImage, width: u32, height: u32) -> DynamicImage {
     img.crop(crop_x, crop_y, width, height)
 }
 
-fn main() {
-    println!("Loading background image");
-    let img = image::open(std::env::args().nth(1).unwrap()).unwrap();
-    println!("Loading overlay");
-    let overlay = image::open(std::env::args().nth(2).unwrap()).unwrap();
-
+fn overlay_frame(
+    img: &DynamicImage,
+    overlay: &DynamicImage,
+    width: u32,
+    height: u32,
+) -> DynamicImage {
     println!("Resizing background image");
-    let mut img = resize_crop(img, 1024, 1024);
+    let mut img = resize_crop(img.clone(), width, height);
 
     println!("Resizing overlay");
-    let overlay = resize_crop(overlay, 1024, 1024);
+    let overlay = resize_crop(overlay.clone(), width, height);
 
     println!("Compositing images");
     imageops::overlay(&mut img, &overlay, 0, 0);
+
+    img
+}
+
+fn load_and_composite(
+    image_path: &str,
+    overlay_path: &str,
+    width: u32,
+    height: u32,
+) -> Result<DynamicImage, ImageError> {
+    println!("Loading background image");
+    let img = image::open(image_path)?;
+    println!("Loading overlay");
+    let overlay = image::open(overlay_path)?;
+
+    let img = overlay_frame(&img, &overlay, width, height);
+    Ok(img)
+}
+
+fn main() {
+    let img = load_and_composite(
+        std::env::args().nth(1).unwrap().as_str(),
+        std::env::args().nth(2).unwrap().as_str(),
+        1024,
+        1024,
+    )
+    .unwrap();
 
     println!("Saving PNG");
     img.save_with_format("./result.png", ImageFormat::Png)
