a9fcc2193a27906956fb887f5c9e73d1ff8a96b7
David Fisher <david@actbluetech.com>
2022-12-16 13:02:02 -0500

    Add two basic tests

--- src/main.rs
+++ src/main.rs
@@ -174,3 +174,18 @@ async fn main() {
         .await
         .unwrap();
 }
+
+#[test]
+fn test_resize_crop() {
+    let img = image::DynamicImage::new_rgba8(200, 100);
+    let expected_img = image::DynamicImage::new_rgba8(100, 100);
+    assert_eq!(resize_crop(img, 100, 100), expected_img);
+}
+
+#[test]
+fn test_overlay_frame() {
+    let img = image::DynamicImage::new_rgba8(200, 100);
+    let overlay = image::DynamicImage::new_rgba8(50, 50);
+    let expected_img = image::DynamicImage::new_rgba8(100, 100);
+    assert_eq!(overlay_frame(&img, &overlay, 100, 100), expected_img);
+}
