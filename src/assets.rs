1487f2986b5c96af7d29b42100916bd223d437a0
Nat Budin <nbudin@actbluetech.com>
2022-12-17 08:16:59 -0800

    Rework how overlays are structured; add API endpoint

--- src/assets.rs
+++ src/assets.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use image::DynamicImage;
 use once_cell::sync::Lazy;
 
@@ -5,12 +7,23 @@ static ABTWU_TOP_LEFT_TEMPLATE: Lazy<DynamicImage> = Lazy::new(|| {
     image::load_from_memory(include_bytes!("../assets/abtwu_top_left_template.png")).unwrap()
 });
 
-pub enum ImageAssetIdentity {
-    ABTWUTopLeftTemplate,
+pub struct Overlay {
+    pub key: &'static str,
+    pub description: &'static str,
+    pub image: &'static DynamicImage,
 }
 
-pub fn get_image(identity: ImageAssetIdentity) -> &'static DynamicImage {
-    match identity {
-        ImageAssetIdentity::ABTWUTopLeftTemplate => &ABTWU_TOP_LEFT_TEMPLATE,
-    }
-}
+pub static OVERLAYS: Lazy<Vec<Overlay>> = Lazy::new(|| {
+    vec![Overlay {
+        key: "abtwu-top-left-template",
+        description: "ActBlue Tech Workers Union top left banner",
+        image: &ABTWU_TOP_LEFT_TEMPLATE,
+    }]
+});
+
+pub static OVERLAYS_BY_KEY: Lazy<HashMap<&str, &Overlay>> = Lazy::new(|| {
+    OVERLAYS
+        .iter()
+        .map(|overlay| (overlay.key, overlay))
+        .collect()
+});
