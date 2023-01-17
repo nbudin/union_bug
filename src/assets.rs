dfee3b4809d9e849646854999ba33e496cfa37fe
Nat Budin <nbudin@actbluetech.com>
2023-01-17 10:23:49 -0800

    Add AB1U logo template

--- src/assets.rs
+++ src/assets.rs
@@ -3,6 +3,9 @@ use std::collections::HashMap;
 use image::DynamicImage;
 use once_cell::sync::Lazy;
 
+static AB1U_TOP_LEFT_TEMPLATE: Lazy<DynamicImage> = Lazy::new(|| {
+    image::load_from_memory(include_bytes!("../assets/ab1u_top_left_template.png")).unwrap()
+});
 static ABTWU_TOP_LEFT_TEMPLATE: Lazy<DynamicImage> = Lazy::new(|| {
     image::load_from_memory(include_bytes!("../assets/abtwu_top_left_template.png")).unwrap()
 });
@@ -14,11 +17,18 @@ pub struct Overlay {
 }
 
 pub static OVERLAYS: Lazy<Vec<Overlay>> = Lazy::new(|| {
-    vec![Overlay {
-        key: "abtwu-top-left-template",
-        description: "ActBlue Tech Workers Union top left banner",
-        image: &ABTWU_TOP_LEFT_TEMPLATE,
-    }]
+    vec![
+        Overlay {
+            key: "abtwu-top-left-template",
+            description: "ActBlue Tech Workers Union top left banner",
+            image: &ABTWU_TOP_LEFT_TEMPLATE,
+        },
+        Overlay {
+            key: "ab1u-top-left-template",
+            description: "ActBlue Union top left banner",
+            image: &AB1U_TOP_LEFT_TEMPLATE,
+        },
+    ]
 });
 
 pub static OVERLAYS_BY_KEY: Lazy<HashMap<&str, &Overlay>> = Lazy::new(|| {
