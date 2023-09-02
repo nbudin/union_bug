da33f36113e3db3725ee35e5746fdf1b8affec1a
Nat Budin <nbudin@actbluetech.com>
2023-09-02 11:26:13 -0700

    My bash is real good

--- build_vars.sh
+++ build_vars.sh
@@ -6,7 +6,7 @@ export RUST_TARGET=$CPU_ARCH-unknown-linux-gnu
 export DPKG_ARCH=$TARGETARCH
 export PKG_CONFIG_SYSROOT_DIR=/usr/lib/$CPU_ARCH-linux-gnu
 export PKG_CONFIG_PATH=$PKG_CONFIG_SYSROOT_DIR/pkgconfig
-if [ $TARGETARCH -ne $BUILDARCH ]
+if [ $TARGETARCH != $BUILDARCH ]
 then
   export GCC_PKG=gcc-12-${CPU_ARCH/x86_64/x86-64}-linux-gnu
 fi
