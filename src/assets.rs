a00d637eaee55651235123246c6828d2d2e0506d
Nat Budin <nbudin@actbluetech.com>
2024-03-26 13:01:55 -0700

    Add fair contract now image

--- src/assets.rs
+++ src/assets.rs
@@ -12,6 +12,9 @@ static ABTWU_TOP_LEFT_TEMPLATE: Lazy<DynamicImage> = Lazy::new(|| {
 static ABTWU_RED_TOP_LEFT_TEMPLATE: Lazy<DynamicImage> = Lazy::new(|| {
     image::load_from_memory(include_bytes!("../assets/abtwu_red_top_left_template.png")).unwrap()
 });
+static ABTWU_RED_FAIR_CONTRACT_NOW: Lazy<DynamicImage> = Lazy::new(|| {
+    image::load_from_memory(include_bytes!("../assets/abtwu_red_fair_contract_now.png")).unwrap()
+});
 
 pub struct Overlay {
     pub key: &'static str,
@@ -30,6 +33,11 @@ pub static OVERLAYS: Lazy<Vec<Overlay>> = Lazy::new(|| {
             key: "abtwu-red-top-left-template",
             description: "ActBlue Tech Workers Union red top left banner",
             image: &ABTWU_RED_TOP_LEFT_TEMPLATE,
+        },
+        Overlay {
+            key: "abtwu-red-fair-contract-now",
+            description: "ActBlue Tech Workers Union red Fair Contract Now",
+            image: &ABTWU_RED_FAIR_CONTRACT_NOW,
         },
         Overlay {
             key: "ab1u-top-left-template",
